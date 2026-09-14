23576bdfcc2ced4d39bac896b304c078ba3a1dfc
Ismael Costa <ismaelirc@gmail.com>
2026-09-14 10:14:06 -0300

    (doc): Removendo arquivos e incluindo configurações do gitignore e .vscode

--- src/features/vagas/VagaForm.jsx
+++ src/features/vagas/VagaForm.jsx
@@ -2,27 +2,59 @@ export default function VagaForm() {
   return (
     <form className="bg-white rounded-xl p-6 shadow-sm">
       <div className="mb-4">
-        <label className="block mb-2 font-medium">Nome</label>
+        <label className="block mb-2 font-medium">Link Vaga</label>
 
         <input
           type="text"
           className="w-full rounded-lg border border-gray-300 px-4 py-2"
-          placeholder="Digite seu nome"
+          placeholder="Link da vaga"
         />
       </div>
 
       <div className="mb-4">
-        <label className="block mb-2 font-medium">Email</label>
+        <label className="block mb-2 font-medium">Empresa</label>
 
         <input
-          type="email"
+          type="text"
           className="w-full rounded-lg border border-gray-300 px-4 py-2"
-          placeholder="Digite seu email"
+          placeholder="Nome da empresa"
         />
       </div>
 
+      <div className="mb-4">
+        <label className="block mb-2 font-medium">Modelo de contrato</label>
+
+        <select
+          type="text"
+          className="w-full rounded-lg border border-gray-300 px-4 py-2"
+          placeholder="Modelo de contrato"
+        >
+          <option>Selecione</option>
+          <option>CLT</option>
+          <option>PJ</option>
+          <option>CLT/PJ</option>
+          <option>Contract</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label className="block mb-2 font-medium">Por onde encontrei</label>
+
+        <select
+          type="text"
+          className="w-full rounded-lg border border-gray-300 px-4 py-2"
+          placeholder="Por onde encontrei a vaga"
+        >
+          <option>Selecione</option>
+          <option>Site Empresa</option>
+          <option>LinkedIn</option>
+          <option>Plataforma de vagas</option>
+          <option>Indicação</option>
+        </select>
+      </div>
+
       <button
-        type="submit"
+        type="button"
         className="rounded-lg bg-blue-600 px-5 py-2 text-white hover:bg-blue-700"
       >
         Salvar
